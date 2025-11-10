a9da62395127d2ea914c6ed95a34d00dbbeed1c7
NemethTamas1 <nemethtamas029@gmail.com>
2025-11-10 13:54:53 +0100

    test model and migration added

---
+++ backend/app/Models/Data.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Data extends Model
+{
+    public $timestamps = true;
+
+    public $table = "datas";
+
+    protected $fillable = [
+        "name",
+        "price"
+    ];
+}
---
+++ backend/database/seeders/DataSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table("datas")->insert([
+            [
+                "name" => "Test 1",
+                "price" => 100
+            ],
+            [
+                "name" => "Test 2",
+                "price" => 200
+            ],
+            [
+                "name" => "Test 3",
+                "price" => 300
+            ]
+        ]);
+    }
+}
