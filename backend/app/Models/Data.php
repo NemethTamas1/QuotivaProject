e54d8197ce57c390d8a12f4fea0adfa4732de3d6
NemethTamas1 <nemethtamas029@gmail.com>
2025-11-16 11:08:27 +0100

    Model uses the correct table, migration configured

--- backend/app/Models/Data.php
+++ backend/app/Models/Data.php
@@ -8,7 +8,7 @@ class Data extends Model
 {
     public $timestamps = true;
 
-    public $table = "datas";
+    public $table = "dev_datas";
 
     protected $fillable = [
         "name",
--- backend/database/migrations/2025_11_10_122941_create_dev_datas_table.php
+++ backend/database/migrations/2025_11_10_122941_create_dev_datas_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('datas', function (Blueprint $table) {
+        Schema::create('dev_datas', function (Blueprint $table) {
             $table->id();
             $table->string("name");
             $table->integer("price");
