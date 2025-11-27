a0795e0e2895dfa3fc9d3cd24fbb4a79fef6fd27
NemethTamas1 <nemethtamas029@gmail.com>
2025-11-27 17:44:53 +0100

    main page updated

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,32 +3,38 @@
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [message, setMessage] = useState<String>("");
+  const [rawData, setRawData] = useState<any>(null);
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    fetch("http://localhost:8000/api/test")
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  console.log("USE EFFECT FUT");
+  console.log("API_URL:", process.env.NEXT_PUBLIC_API_URL);
+
+  fetch(`${apiUrl}/api/datas`)
     .then(res => res.json())
     .then(data => {
-      setMessage(data.message);
+      setRawData(data);
       setLoading(false);
     })
     .catch(err => {
       console.error("Error fetching API:", err);
-      setMessage("Error fetching API");
       setLoading(false);
-    })
-    ,[];
-  });
+    });
+}, []);
 
-  return (
-         <div className="p-4">
-           <h1 className="text-2xl font-bold">Quotiva Frontend</h1>
-           {loading ? (
-             <p>Betöltés...</p>
-           ) : (
-             <p className="text-lg">API üzenet: {message}</p>
-           )}
-         </div>
-       );
+
+return (
+  <div className="p-4">
+    <h1 className="text-2xl font-bold text-center">Quotiva Frontend</h1>
+
+    {loading ? (
+      <p>Betöltés...</p>
+    ) : (
+      <pre style={{ whiteSpace: "pre-wrap" }}>
+        {JSON.stringify(rawData, null, 2)}
+      </pre>
+    )}
+  </div>
+);
 }
