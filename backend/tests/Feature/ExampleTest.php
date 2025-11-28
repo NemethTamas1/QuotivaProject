f1e67d0dca9c44bc399980f5687f4e344a9abe5a
NemethTamas1 <nemethtamas029@gmail.com>
2025-11-28 15:02:22 +0100

    testing environment updated: phpunitxml, feature simple test

--- backend/tests/Feature/ExampleTest.php
+++ backend/tests/Feature/ExampleTest.php
@@ -3,16 +3,21 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
+    use RefreshDatabase;
+
+    public function test_successful_response_on_datas(): void
     {
-        $response = $this->get('/');
+        DB::table('datas')->insert(['name' => 'test', 'price' => 100]);
+        
+        $response = $this->get('/api/datas');
 
         $response->assertStatus(200);
     }
