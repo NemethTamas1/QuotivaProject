f3489f1553e5e580bd0a585f30f3c2b9f2b4f287
NemethTamas1 <nemethtamas029@gmail.com>
2025-12-01 14:39:27 +0100

    Font stlye added globally. Started main page added.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,38 +3,52 @@
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [rawData, setRawData] = useState<any>(null);
-  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-  console.log("USE EFFECT FUT");
-  console.log("API_URL:", process.env.NEXT_PUBLIC_API_URL);
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+    console.log("USE EFFECT FUT");
+    console.log("API_URL:", process.env.NEXT_PUBLIC_API_URL);
+  });
 
-  fetch(`${apiUrl}/api/datas`)
-    .then(res => res.json())
-    .then(data => {
-      setRawData(data);
-      setLoading(false);
-    })
-    .catch(err => {
-      console.error("Error fetching API:", err);
-      setLoading(false);
-    });
-}, []);
+  return (
+    <div className="min-h-screen bg-black text-white flex flex-col">
+      {/* NavBar */}
+      <nav className="w-full py-4 flex justify-between px-3 items-center backdrop-blur-md bg-black">
+        <a href="/" className="text-3xl font-semibold tracking-wide">
+          Quotiva
+        </a>
 
+        <div className="flex gap-6 text-2xl">
+          <a href="#" className="hover:text-green-400 transition">Új árajánlat</a>
+          <a href="#" className="hover:text-green-400 transition">Árajánlataim</a>
+          <a href="#" className="hover:text-green-400 transition">Belépés</a>
+        </div>
+      </nav>
 
-return (
-  <div className="p-4">
-    <h1 className="text-2xl font-extrabold text-green-300 text-center">Quotiva Frontend</h1>
+      {/* Main */}
+      <main className="flex-1 flex items-center justify-center text-center px-4">
+        <div>
+          <h2 className="text-3xl sm:text-5xl font-bold leading-snug">
+            Köszöntünk a Quotiva oldalán,
+            <br /> egy egyszerű, de
+            <span className="text-green-400 drop-shadow-[0_0_10px_#00ff80]">
+              {" "}hatékony
+            </span>
+            {" "}
+            árajánlat készítő oldalon.
+          </h2>
 
-    {loading ? (
-      <p>Betöltés...</p>
-    ) : (
-      <pre style={{ whiteSpace: "pre-wrap" }}>
-        {JSON.stringify(rawData, null, 2)}
-      </pre>
-    )}
-  </div>
-);
+          <p className="mt-6 mx-auto text-white/60 max-w-xl auto">
+            Készítsen gyorsan, tisztán és profin árajánlatokat modern felületen, átlátható tételekkel, és automatikus összesítéssel.
+          </p>
+
+          <div className="mt-10 flex justify-center gap-4">
+            <a href="#" className="px-6 py-3 bg-green-500 text-black font-bold rounded-md hover:bg-green-400 transition shadow-[0_0_15px_#00ff80]">Új árajánlat készítése</a>
+
+            <a href="#" className="px-6 py-3 border border-white/20 rounded-md hover:bg-white/10 transition">Árajánlatok megtekintése</a>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
