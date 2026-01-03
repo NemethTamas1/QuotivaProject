a90545d0119056362defdf5934553b424c69847e
NemethTamas1 <nemethtamas029@gmail.com>
2026-01-03 13:33:28 +0100

    OfferItemsTable is implemented into create offer page.

--- frontend/app/createoffer/page.tsx
+++ frontend/app/createoffer/page.tsx
@@ -3,17 +3,14 @@
 import { useState, useEffect } from "react";
 import { useForm, useFieldArray, SubmitHandler, FormProvider } from "react-hook-form";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
-
 import CustomerDetails from "./components/CustomerDetails";
 import OfferDetails from "./components/OfferDetails";
 
 
-import { OfferItem, type CreateOfferForm, type OfferItemInput } from "./types";
+import { OfferItem, type CreateOfferForm } from "./types";
 import EditItem from "../components/EditItem";
 import OfferItemDetails from "./components/OfferItemDetails";
+import OfferItemsTable from "./components/OfferItemsTable";
 
 export default function CreateOfferPage() {
     const methods = useForm<CreateOfferForm>({
@@ -25,7 +22,9 @@ export default function CreateOfferPage() {
         },
     });
 
-    const { register, handleSubmit, control } = methods;
+    const { register, handleSubmit } = methods;
+
+    const { control } = useForm<CreateOfferForm>();
 
     const { fields, append, remove, update } = useFieldArray({
         control,
@@ -70,14 +69,10 @@ export default function CreateOfferPage() {
         console.log("API_URL:", process.env.NEXT_PUBLIC_API_URL);
     }, []);
 
-    // Field figyelő
-    useEffect(() => {
-        console.log("Fields changed: ", fields);
-    }, [fields]);
-
     const [editingItem, setEditingItem] =
         useState<OfferItem | null>(null);
 
+    
 
     return (
         <div className="min-h-screen bg-black text-white flex flex-col">
@@ -92,80 +87,8 @@ export default function CreateOfferPage() {
 
                         <OfferItemDetails append={append} />
 
-                        {/* T É T E L E K  T Á B L Á Z A T A */}
-                        <div className="relative w-full rounded-md">
-
-                            <div className="absolute -inset-1.5 bg-green-500 rounded-md blur-sm opacity-25"></div>
-
-                            <div className="relative rounded-md overflow-hidden bg-slate-900">
-                                <table className="w-full border-collapse">
-                                    <thead>
-                                        <tr>
-                                            <th className="text-green-500 p-3 text-lg">Tétel megnevezés</th>
-                                            <th className="text-green-500 p-3 text-lg">Mennyiség</th>
-                                            <th className="text-green-500 p-3 text-lg">Munkadíj egységár</th>
-                                            <th className="text-green-500 p-3 text-lg">Anyag egységár</th>
-                                            <th className="text-green-500 p-3 text-lg">Munkadíj</th>
-                                            <th className="text-green-500 p-3 text-lg">Anyagdíj</th>
-                                            <th className="text-green-500 p-3 text-lg">Műveletek</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {fields.length === 0 ? (
-                                            <tr>
-                                                <td colSpan={7} className="text-center py-10 text-lg text-slate-400">
-                                                    Nincsenek még hozzáadott tételek.
-                                                </td>
-                                            </tr>
-                                        ) : (
-                                            fields.map((field) => (
-                                                <tr key={field.id} className="border-t text-center odd:bg-white even:bg-green-200">
-                                                    <td className="p-2 text-lg text-black">{field.name}</td>
-                                                    <td className="p-2 text-lg text-black">{field.quantity} Ft</td>
-                                                    <td className="p-2 text-lg text-black">{field.labor_unit_price} Ft</td>
-                                                    <td className="p-2 text-lg text-black">{field.material_unit_price} Ft</td>
-                                                    <td className="p-2 text-lg text-black">{field.quantity * field.labor_unit_price} Ft</td>
-                                                    <td className="p-2 text-lg text-black">{field.quantity * field.material_unit_price} Ft</td>
-                                                    <td className="p-2 text-lg text-black">
-                                                        <FontAwesomeIcon
-                                                            icon={faPenToSquare}
-                                                            size="xl"
-                                                            className="text-white bg-green-500 rounded-md py-1"
-                                                            onClick={() => {
-                                                                setEditingItem({
-                                                                    _fieldId: field.id,
-                                                                    name: field.name,
-                                                                    quantity: field.quantity,
-                                                                    quantity_type: field.quantity_type,
-                                                                    labor_unit_price: field.labor_unit_price,
-                                                                    material_unit_price: field.material_unit_price,
-                                                                })
-                                                            }} />
-                                                        <FontAwesomeIcon
-                                                            icon={faTrash}
-                                                            size="xl"
-                                                            className="text-white bg-red-500 rounded-md py-1"
-                                                            onClick={() => {
-                                                                const index = fields.findIndex(
-                                                                    (f) => f.id === field.id
-                                                                );
-
-                                                                if (index !== -1) {
-                                                                    remove(index);
-                                                                }
-                                                            }} />
-                                                    </td>
-                                                </tr>
-                                            ))
-                                        )}
-                                    </tbody>
-
-                                </table>
-                            </div>
-                        </div>
-
-
-
+                        <OfferItemsTable fields={fields} remove={remove} setEditingItem={setEditingItem}/>
+                        
                         <button type="submit" className="w-2/12 bg-green-600 text-black font-semibold text-lg py-3 rounded hover:bg-green-700 transition my-7"
                         >
                             Ajánlat létrehozása
