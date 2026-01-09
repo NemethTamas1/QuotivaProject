0129d93fc67503c249d8042deec8b69cd419d2ac
NemethTamas1 <nemethtamas029@gmail.com>
2026-01-09 10:56:06 +0100

    axios settings updated. not used yet.

--- frontend/lib/http.tsx
+++ frontend/lib/http.tsx
@@ -3,8 +3,10 @@ import axios from "axios";
 const http = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     withCredentials: true,
+    xsrfCookieName: "XSRF-TOKEN",
+    xsrfHeaderName: "X-XSRF-TOKEN",
     headers: {
-        Accept: 'application/json',
+        "Content-Type": 'application/json',
     }
 });
 
