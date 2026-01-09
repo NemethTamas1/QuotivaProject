93424e9f8c8571df4c3549875eefbd56aa5f67b9
NemethTamas1 <nemethtamas029@gmail.com>
2026-01-09 10:52:32 +0100

    app.php extended. frontend requests are stateful

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\HandleCors;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(HandleCors::class);
+        $middleware->api(prepend: [
+            EnsureFrontendRequestsAreStateful::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
