ce1a6043c9810f5144d86009d3b9e0281395020d
NemethTamas1 <nemethtamas029@gmail.com>
2026-01-09 10:54:36 +0100

    navbar now checks if the user is logged in or not

--- frontend/app/components/NavBar.tsx
+++ frontend/app/components/NavBar.tsx
@@ -1,12 +1,67 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Link from "next/link"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { whoAmI } from "@/lib/auth"
+import { useRouter } from "next/navigation"
 
 export default function NavBar() {
+
+
+
+  const handleCreateOfferLink = async () => {
+    try {
+      await whoAmI();
+      router.push("/createoffer");
+    } catch (error) {
+      router.push("/login");
+    }
+  };
+
+  const handleLogOut = async () => {
+    try {
+      const api = process.env.NEXT_PUBLIC_API_URL;
+
+      // CSRF TOKEN
+      await fetch(`${api}/sanctum/csrf-cookie`, {
+        credentials: 'include'
+      });
+      const token = decodeURIComponent(
+        document.cookie
+          .split('; ')
+          .find(c => c.startsWith('XSRF-TOKEN='))
+          ?.split('=')[1] ?? ''
+      );
+
+      const res = await fetch(`${api}/logout`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRF-TOKEN': token,
+        }
+      });
+      alert("sikeres kijelentkezés")
+      setIsLoggedIn(false);
+      router.push("/");
+
+    } catch (error) {
+      console.error("kijelentkezési hiba: ", error)
+    }
+  };
+
   const [isOpen, setIsOpen] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    whoAmI()
+      .then(() => setIsLoggedIn(true))
+      .catch(() => setIsLoggedIn(false))
+  }, [])
 
   return (
     <>
@@ -16,9 +71,17 @@ export default function NavBar() {
         </Link>
 
         <div className="hidden md:flex gap-6 text-2xl">
-          <Link href="/createoffer">Új árajánlat</Link>
+          <button onClick={handleCreateOfferLink}>Új árajánlat</button>
+
           <Link href="/offers">Árajánlataim</Link>
-          <Link href="/login">Belépés</Link>
+
+          {isLoggedIn === null ? null : isLoggedIn ? (
+            <button onClick={handleLogOut}>Kijelentkezés</button>
+          ) : (
+            <Link href="/login">Belépés</Link>
+          )
+          }
+
         </div>
 
         <button onClick={() => setIsOpen(!isOpen)} className="md:hidden">
@@ -29,8 +92,20 @@ export default function NavBar() {
       {isOpen && (
         <div className="md:hidden bg-black border-t border-white/10">
           <div className="flex flex-col gap-4 px-4 py-4 text-xl">
-            <Link href="/offers/new">Új árajánlat</Link>
+            {isLoggedIn ? (
+              <button onClick={handleCreateOfferLink}>Új árajánlat</button>
+            ) : (
+              <button onClick={() => router.push("/login")}>Új árajánlat</button>
+            )}
+
+            {isLoggedIn ? (
+              <button onClick={handleLogOut}>Kijelentkezés</button>
+            ) : (
+              <Link href="/login">Belépés</Link>
+            )}
+
             <Link href="/offers">Árajánlataim</Link>
+
             <Link href="/login">Belépés</Link>
           </div>
         </div>
