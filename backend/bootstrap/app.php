df489e459156bc21a743c3c6e37bdfcacde81661
NemethTamas1 <nemethtamas029@gmail.com>
2026-01-12 15:24:05 +0100

    validatecsrf token and statful api added

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -10,22 +10,22 @@ use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(HandleCors::class);
-
-        $middleware->web(prepend: [
-            EnsureFrontendRequestsAreStateful::class
+        $middleware->validateCsrfTokens(except: [
+            'login',
+            'logout',
+            'api/login',
+            'api/logout',
+            'api/me',
+            'sanctum/csrf-cookie'
         ]);
 
-        $middleware->web(append: [
-            StartSession::class,
-            VerifyCsrfToken::class
-        ]);
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
