e6e8a39ba2184257ed79d34df901be19e68b71b4
NemethTamas1 <nemethtamas029@gmail.com>
2026-01-19 13:29:21 +0100

    register added to app.php

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'login',
             'logout',
+            'api/register',
             'api/login',
             'api/logout',
             'api/me',
