3ef3ef9da5cc186c17ddfe9f1021e1fd280ed63f
NemethTamas1 <nemethtamas029@gmail.com>
2026-01-22 16:41:19 +0100

    HomePage now loads user and greets him/her

--- frontend/app/dashboard/components/HomePage.tsx
+++ frontend/app/dashboard/components/HomePage.tsx
@@ -1,10 +1,38 @@
 'use client';
 
-export default function HomePage(){
+import { useEffect, useState } from "react";
+import { whoAmI } from "@/lib/auth";
+
+interface User {
+    name: string
+}
+
+export default function HomePage() {
+
+    const [user, setUser] = useState<User>();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const userData = await whoAmI();
+                setUser(userData);
+            } catch (error) {
+                console.error("Nem sikerült lekérni a felhasználót:", error);
+            } finally{
+                setLoading(false);
+            }
+        };
+
+        fetchUser();
+    }, []);
+
+    if (!user) return null;
 
     return (
+
         <>
-            <h1 className="text-2xl text-center my-36">HomePage</h1>
+            <h1 className="text-3xl p-6">Üdvözöljük, {user.name}👋</h1>
         </>
     );
 }
