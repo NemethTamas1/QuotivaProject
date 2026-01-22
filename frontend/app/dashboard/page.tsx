04afc481ff797bf99a8f3898f9500568e64ce74a
NemethTamas1 <nemethtamas029@gmail.com>
2026-01-22 16:40:58 +0100

    active selected menu now changes color

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import HomePage from "./components/HomePage";
 import MyProfiles from "./components/MyProfiles";
-import Home from "../page";
 
 export default function Dashboard(){
 
@@ -13,16 +12,17 @@ export default function Dashboard(){
         <>
             <div className="w-full flex flex-row lg:min-h-screen">
                 {/* Bal oldali oldalsáv */}
-                <div className="w-1/12 bg-red-400">
-                    <button onClick={() => setSelectedPage('Home')} className="block text-2xl p-2">HomePage</button>
-                    <button onClick={() => setSelectedPage('myProfiles')} className="block text-2xl p-2">Profiljaim</button>
+                <div className="w-1/12 bg-[#27272A] relative z-10 shadow-[12px_0_24px_-12px_#18181B]">
+                    <button onClick={() => setSelectedPage('Home')} className={`block text-2xl p-2 ${selectedPage === 'Home' ? 'text-green-400' : 'text-gray-200'}`}>HomePage</button>
+                    
+                    <button onClick={() => setSelectedPage('myProfiles')} className={`block text-2xl p-2 ${selectedPage === 'myProfiles' ? 'text-green-400' : 'text-gray-200'}`}>Profiljaim</button>
                 </div>
 
 
 
 
                 {/* Jobb oldali fő tartalom */}
-                <div className="w-11/12 bg-blue-400">
+                <div className="w-11/12 bg-[#27272A]">
                     {selectedPage === 'Home' && (
                         <HomePage />
                     )}
