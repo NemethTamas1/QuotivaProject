9369429b6f9e2394f4dc449e1e473b6314c473c0
NemethTamas1 <nemethtamas029@gmail.com>
2026-01-25 17:26:53 +0100

    web.php cleaned

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,26 +4,26 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::post('/login', [AuthController::class, 'login'])->middleware('web');
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::post("/logout", [AuthController::class, "logout"])->middleware('web');
+Route::post("/logout", [AuthController::class, "logout"]);
 
-Route::get('/csrf-test', function () {
-    return response()->json([
-        'csrf' => csrf_token(),
-    ]);
-});
+// Route::get('/csrf-test', function () {
+//     return response()->json([
+//         'csrf' => csrf_token(),
+//     ]);
+// });
 
-Route::post('/csrf-test', function (Request $request) {
-    return response()->json([
-        'ok' => true,
-        'token' => csrf_token(),
-    ]);
-});
+// Route::post('/csrf-test', function (Request $request) {
+//     return response()->json([
+//         'ok' => true,
+//         'token' => csrf_token(),
+//     ]);
+// });
 
 // Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
 //     return $request->user();
