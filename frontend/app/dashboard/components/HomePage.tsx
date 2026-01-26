8ebc99276d2e23432c3708854ddcf4ca895947a3
NemethTamas1 <nemethtamas029@gmail.com>
2026-01-26 15:39:42 +0100

    homepage refactored. it now only watches the AuthContext to see if a user is logged in or not

--- frontend/app/dashboard/components/HomePage.tsx
+++ frontend/app/dashboard/components/HomePage.tsx
@@ -1,38 +1,16 @@
 'use client';
 
-import { useEffect, useState } from "react";
-import { whoAmI } from "@/lib/auth";
+import { useAuth } from "@/context/AuthContext";
 
-interface User {
-    name: string
-}
 
 export default function HomePage() {
 
-    const [user, setUser] = useState<User>();
-    const [loading, setLoading] = useState(true);
+    const { user } = useAuth();
 
-    useEffect(() => {
-        const fetchUser = async () => {
-            try {
-                const userData = await whoAmI();
-                setUser(userData);
-            } catch (error) {
-                console.error("Nem sikerült lekérni a felhasználót:", error);
-            } finally{
-                setLoading(false);
-            }
-        };
-
-        fetchUser();
-    }, []);
-
-    if (!user) return null;
 
     return (
-
         <>
-            <h1 className="text-3xl p-6">Üdvözöljük, {user.name}👋</h1>
+            <h1 className="text-3xl p-6">Üdvözöljük, {user?.name}👋</h1>
         </>
     );
 }
