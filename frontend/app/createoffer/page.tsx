77a2dc127e056c531f31f66a31e6869e32c3e00d
NemethTamas1 <nemethtamas029@gmail.com>
2026-01-26 15:39:14 +0100

    useless cookie request removed

--- frontend/app/createoffer/page.tsx
+++ frontend/app/createoffer/page.tsx
@@ -44,7 +44,6 @@ export default function CreateOfferPage() {
     const onSubmit: SubmitHandler<CreateOfferForm> = async (data) => {
 
         try {
-            await http.get(`/sanctum/csrf-cookie`);
 
             const response = await http.post(`/api/offers`, data);
 
