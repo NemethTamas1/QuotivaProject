9a6afdfb749d8f1e678703f54d1163cc0f241fac
NemethTamas1 <nemethtamas029@gmail.com>
2026-01-26 15:37:00 +0100

    routes organized, also web.php cleared up

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DataController;
 use App\Http\Controllers\OfferController;
 use App\Http\Controllers\RegisterController;
@@ -7,23 +8,22 @@ use App\Http\Controllers\UserProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
-Route::get("/test", function () {
-    return response()->json(["message" => "API teszt backendről"]);
-});
-
-Route::apiResource("/datas", DataController::class);
-
-
-Route::post("/register", [RegisterController::class, "store"]);
-
 Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
     return response()->json($request->user());
 });
 
 Route::middleware('auth:sanctum')->apiResource("/offers", OfferController::class);
 Route::middleware('auth:sanctum')->apiResource('/user-profiles', UserProfileController::class);
+
+// A u t h
+Route::post("/register", [RegisterController::class, "store"]);
+
+Route::post("/logout", [AuthController::class, "logout"]);
+
+Route::post("/authenticate", [AuthController::class, "authenticate"])->name("auth.authenticate");
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+// ----------------------------------------------------------------------------------------------------
+
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,34 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::post("/logout", [AuthController::class, "logout"]);
-
-// Route::get('/csrf-test', function () {
-//     return response()->json([
-//         'csrf' => csrf_token(),
-//     ]);
-// });
-
-// Route::post('/csrf-test', function (Request $request) {
-//     return response()->json([
-//         'ok' => true,
-//         'token' => csrf_token(),
-//     ]);
-// });
-
-// Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
-//     return $request->user();
-// });
-
-// Route::get("/whoami", function(Request $request){
-//     return $request->user() ?: 'NO USER';
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
