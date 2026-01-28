00a19988a5e650d2cf10cd091130b4264c133d53
NemethTamas1 <nemethtamas029@gmail.com>
2026-01-28 14:14:50 +0100

    login page is now responsive on mobile

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -32,11 +32,11 @@ export default function LoginPage() {
         <>
         <NavBar />
             <div>
-                <div className="relative w-3/12 mx-auto lg:my-20">
+                <div className="relative w-10/12 lg:w-3/12 my-36 mx-auto lg:my-20">
 
                     <div className="absolute -inset-1.5 bg-green-500 rounded-md blur-sm opacity-25 z-0"></div>
                     <div className="relative bg-gray-900 rounded-md mx-auto">
-                        <h1 className="text-center text-3xl text-green-400 my-5 lg:pt-5 font-semibold" style={{
+                        <h1 className="text-center text-3xl text-green-400 my-5 pt-8 lg:pt-5 font-semibold" style={{
                             textShadow: "0px 0px 20px rgba(34, 197, 94, 1"
                         }}>Bejelentkezés</h1>
 
@@ -49,7 +49,7 @@ export default function LoginPage() {
                                 <p className="ml-auto text-green-400 mb-5">Elfelejtett jelszó?</p>
                             </div>
 
-                            <button onClick={handleLogin} className="bg-green-400 mx-auto p-4 rounded-md lg:mb-5 text-black text-lg font-semibold">Bejelentkezés</button>
+                            <button onClick={handleLogin} className="bg-green-400 mx-auto p-4 rounded-md mb-5 text-black text-lg font-semibold">Bejelentkezés</button>
 
                             <p className="text-center mb-5">Nincs fiókod? <Link href="/register" className="text-green-400">Regisztrálj itt</Link></p>
                         </div>
