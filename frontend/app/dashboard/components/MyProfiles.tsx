a8bbe7961a0b7e482c3495a6da7629a3e04ea24f
NemethTamas1 <nemethtamas029@gmail.com>
2026-01-29 15:26:59 +0100

    User can now select which profile they want.

--- frontend/app/dashboard/components/MyProfiles.tsx
+++ frontend/app/dashboard/components/MyProfiles.tsx
@@ -4,6 +4,7 @@ import http from "@/lib/http";
 import { profileType } from "../types/types";
 import ProfileCompiler from "./ProfileCompiler";
 import { useEffect, useState } from "react";
+import { useAuth } from "@/context/AuthContext";
 
 export default function MyProfiles() {
 
@@ -11,6 +12,7 @@ export default function MyProfiles() {
     const [profiles, setProfiles] = useState<profileType[]>([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
+    const { setSelectedUserProfile, selectedUserProfile } = useAuth();
 
     const handleProfileSave = async (data: Partial<profileType>) => {
         try {
@@ -48,6 +50,16 @@ export default function MyProfiles() {
         }
     };
 
+    const saveProfile = async (profile: profileType) => {
+        setSelectedUserProfile(profile);
+        alert("Alprofil sikeresen kiválasztva.")
+    };
+
+    const showCurrentProfile = () => {
+        if(selectedUserProfile) alert("Jelenlegi user profil: " + selectedUserProfile?.company_name);
+        else alert("Nincs kiválasztott profil!");
+    };
+
 
     useEffect(() => {
         fetchProfiles();
@@ -64,14 +76,17 @@ export default function MyProfiles() {
                 >
                     + Új profil létrehozása
                 </button>
+
+                {/* KIVENNI MAJD, EZ CSAK HELPER */}
+                <button onClick={() => showCurrentProfile()} className="bg-orange-400 text-black px-6 py-2 rounded-lg transition-all">Ki vagyok én?</button>
             </div>
 
             <div>
-                {/* Profilok listázása ide jön majd */}
+                {/* Profilok listázása */}
                 {!isLoading && !error && profiles.length > 0 && (
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4 grid-flow-row mx-auto">
                         {profiles.map((p) => (
-                            <div key={p.id} className="rounded-xl bg-[#27272A] p-4 shadow-[0_0_25px_0_#18181B]">
+                            <div key={p.id} className={`rounded-md w-full p-4 shadow-lg transition-all ${selectedUserProfile?.id === p.id ? "ring-2 ring-green-400 bg-[#303036]" : "bg-[#27272A]"}`}>
                                 <div className="font-semibold text-lg">{p.company_name}</div>
                                 <div className="text-sm text-gray-300">
                                     {p.city} {p.zip} • {p.street} {p.house_number}
@@ -79,6 +94,17 @@ export default function MyProfiles() {
                                 <div className="text-sm text-gray-300">
                                     {p.company_email} • {p.company_phone}
                                 </div>
+
+                                {selectedUserProfile === p ? (
+                                    <button className="disabled p-2 bg-green-300 text-black rounded-md hover:bg-green-400 transition-colors">Kiválasztva</button>
+                                ) : (
+                                    <button
+                                        onClick={() => saveProfile(p)}
+                                        className="p-2 bg-green-300 text-black rounded-md hover:bg-green-400 transition-colors"
+                                    >
+                                        Profil kiválasztása
+                                    </button>
+                                )}
                             </div>
                         ))}
                     </div>
