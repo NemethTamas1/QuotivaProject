7bc75b7371389688799b3c6cb81100559a21c66d
NemethTamas1 <nemethtamas029@gmail.com>
2026-01-30 14:47:57 +0100

    createoffer page now displays which user profile is selected

--- frontend/app/createoffer/components/CustomerDetails.tsx
+++ frontend/app/createoffer/components/CustomerDetails.tsx
@@ -2,18 +2,50 @@
 
 import type { UseFormRegister } from "react-hook-form";
 import type { CreateOfferForm } from "@/app/createoffer/types";
+import { useAuth } from "@/context/AuthContext";
+import { profileType } from "@/app/dashboard/types/types";
+import { useEffect, useState } from "react";
+import http from "@/lib/http";
 
 interface Props {
     register: UseFormRegister<CreateOfferForm>;
 }
 
+
 export default function CustomerDetails({ register }: Props) {
+    const {selectedUserProfile, setSelectedUserProfile} = useAuth();
+
+    const fetchProfiles = async () => {
+        try {
+            const res = await http.get('/api/user-profiles');
+            const items: profileType[] = res.data?.data ?? [];
+
+            if (items.length > 0 && !selectedUserProfile) setSelectedUserProfile(items[0]);
+        } catch (e) {
+            console.error("Hiba történt a profilok betöltésekor.");
+        }
+    };
+
+    useEffect(() => {
+        fetchProfiles()
+    }, [])
     return (
         <>
             {/* M E G R E N D E L Ő   A D A T O K*/}
-            <h2 className="text-2xl font-semibold text-green-400 mb-7" style={{
-                textShadow: "0px 0px 20px rgba(34, 197, 94, 1"
-            }}>Megrendelő adatai</h2>
+            <div className="flex flex-row justify-between items-baseline mb-7">
+                <h2 className="text-2xl font-semibold text-green-400" style={{
+                    textShadow: "0px 0px 20px rgba(34, 197, 94, 1"
+                }}>
+                    Megrendelő adatai
+                </h2>
+
+                <div className="flex items-center gap-2">
+                    <span className="text-sm text-gray-400 font-medium italic">Profil:</span>
+                    <span className="text-lg font-bold text-green-400 bg-green-400/10 px-3 py-1 rounded-full border border-green-400/20 shadow-[0_0_15px_rgba(34,197,94,0.2)]">
+                        {selectedUserProfile?.company_name || "Nincs kiválasztva"}
+                    </span>
+                </div>
+            </div>
 
             <div className="relative">
                 <div className="absolute -inset-1.5 bg-green-500 rounded-md blur-sm opacity-25"></div>
