01335dfed784ed3cb7d31e30b0a707ad43419102
NemethTamas1 <nemethtamas029@gmail.com>
2026-02-01 16:56:31 +0100

    Some Offer tests have been written

--- backend/tests/Feature/OffersTest.php
+++ backend/tests/Feature/OffersTest.php
@@ -2,24 +2,50 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use App\Models\UserProfile;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class OffersTest extends TestCase
 {
-    use RefreshDatabase;
+    use DatabaseTransactions;
 
-    public function test_example(): void
+    public function test_can_get_all_offers(): void
     {
-        $response = $this->get('/api/offers');
+        $user = User::create([
+            'name' => "Admin",
+            'email' => "adminemail@gmail.com",
+            'password' => "adminpassword",
+            'role' => "admin"
+        ]);
+
+        $response = $this->actingAs($user)->get('/api/offers');
 
         $response->assertStatus(200);
     }
 
     public function test_can_create_offer(): void
     {
+        $user = User::factory()->create();
+
+        $profile = UserProfile::create([
+            "user_id" => 1,
+            "company_name" => "TesztProfil Kft.",
+            "tax_number" => 12345678 - 1 - 12,
+            "company_email" => "tesztprofilkft@gmail.com",
+            "city" => "Budapest",
+            "zip" => 1234,
+            "street" => "Teszt utca",
+            "house_number" => "12/B",
+            "company_phone" => "+36901346798"
+        ]);
+
         $offerData = [
+            "user_profile_id" => $profile->user_id,
+            "offer_number" => "OFF123456",
             "offer_name" => "Test offer",
             "status" => "pending",
             "dated" => "2025-12-30",
@@ -62,7 +88,7 @@ class OffersTest extends TestCase
             ]
         ];
 
-        $response = $this->postJson('/api/offers', $offerData);
+        $response = $this->actingAs($user)->postJson('/api/offers', $offerData);
 
         $response
             ->assertStatus(201)
@@ -75,7 +101,23 @@ class OffersTest extends TestCase
 
     public function test_created_offer_contains_must_have_data(): void
     {
+        $user = User::factory()->create();
+
+        $profile = UserProfile::create([
+            "user_id" => 1,
+            "company_name" => "TesztProfil Kft.",
+            "tax_number" => 12345678 - 1 - 12,
+            "company_email" => "tesztprofilkft@gmail.com",
+            "city" => "Budapest",
+            "zip" => 1234,
+            "street" => "Teszt utca",
+            "house_number" => "12/B",
+            "company_phone" => "+36901346798"
+        ]);
+
         $offerData = [
+            "user_profile_id" => $profile->user_id,
+            "offer_number" => "OFF123456",
             "offer_name" => "Test offer",
             "status" => "pending",
             "dated" => "2025-12-30",
@@ -84,6 +126,7 @@ class OffersTest extends TestCase
             "tax_percent" => 27,
 
             "client_name" => "Teszt Kft.",
+            "client_email" => "tesztemail@gmail.com",
             "client_phone" => "+3612345678",
             "client_tax_number" => "12345678-1-42",
             "client_zip" => "1011",
@@ -117,7 +160,7 @@ class OffersTest extends TestCase
             ]
         ];
 
-        $response = $this->postJson('/api/offers', $offerData);
+        $response = $this->actingAs($user)->postJson('/api/offers', $offerData);
 
         $response
             ->assertJsonFragment([
