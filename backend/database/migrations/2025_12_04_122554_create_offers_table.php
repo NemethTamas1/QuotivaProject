817e1cedebbfe41f1a20ec222f36ffdfde08a6ec
NemethTamas1 <nemethtamas029@gmail.com>
2026-02-03 16:32:22 +0100

    offers migration consistency updated

--- backend/database/migrations/2025_12_04_122554_create_offers_table.php
+++ backend/database/migrations/2025_12_04_122554_create_offers_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->string("offer_number", 9)->unique();
             $table->string("offer_name", 100);
             $table->string("status", 30);
-            $table->date("dated");
-            $table->date("valid_until");
+            $table->date("dated")->nullable();
+            $table->date("valid_until")->nullable();
             $table->string("currency", 15);
             $table->float("tax_percent");
             $table->float("net_total", 12, 2)->default(0);
@@ -26,8 +26,8 @@ return new class extends Migration
 
             $table->string("client_name", 100);
             $table->string("client_email", 50)->nullable();
-            $table->string("client_phone", 25)->nullable();
-            $table->string("client_tax_number",20)->nullable();
+            $table->string("client_phone", 20)->nullable();
+            $table->string("client_tax_number",25)->nullable();
             $table->string("client_zip",5);
             $table->string("client_city",30);
             $table->string("client_street", 50);
