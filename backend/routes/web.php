42f725aec83042a49d35b90044c23da2b6a15f1d
NemethTamas1 <nemethtamas029@gmail.com>
2026-02-18 13:44:56 +0100

    web.php extended

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Models\Offer;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/response/{id}/{status}', function ($id, $status) {
+    $offer = Offer::findOrFail($id);
+    $offer->update(['status' => $status]);
+
+    return "Köszönjük a válaszát! A rendszert frissítettük.";
 });
