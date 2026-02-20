b4d204d9bf7191fc1b894bba9a06982eb7478b13
NemethTamas1 <nemethtamas029@gmail.com>
2026-02-20 17:19:52 +0100

    navbar for the exceotion

--- frontend/app/createoffer/page.tsx
+++ frontend/app/createoffer/page.tsx
@@ -172,6 +172,7 @@ export default function CreateOfferPage() {
     if (hasNoProfile) {
         return (
             <>
+                <NavBar />
                 <HasNoProfile />
             </>
         );
