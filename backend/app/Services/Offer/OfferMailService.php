e0bdc8aeedfa0e5b251c4f079513973cac13d7e9
NemethTamas1 <nemethtamas029@gmail.com>
2026-04-25 10:06:07 +0200

    signed route fix

--- backend/app/Services/Offer/OfferMailService.php
+++ backend/app/Services/Offer/OfferMailService.php
@@ -5,13 +5,14 @@ namespace App\Services\Offer;
 use App\Mail\OfferEmailToClient;
 use App\Models\Offer;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\URL;
 
 class OfferMailService
 {
     public function sendOfferToClient(Offer $offer): void
     {
-        $acceptUrl = route("offer.accept", ["offer" => $offer->id]);
-        $rejectUrl = route("offer.reject", ["offer" => $offer->id]);
+        $acceptUrl = URL::signedRoute("offer.accept", ["offer" => $offer->id]);
+        $rejectUrl = URL::signedRoute("offer.reject", ["offer" => $offer->id]);
 
         Mail::to($offer->client_email)->send(new OfferEmailToClient($offer, $acceptUrl, $rejectUrl));
     }
